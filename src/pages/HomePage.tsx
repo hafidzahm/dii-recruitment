b4da645751294f4be06795dd0e36ece17ce0b153
Abdul Hafizh <hafizhahm123@gmail.com>
2025-09-30 17:48:24 +0700

    refactoring components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,10 @@
 import {
-  Table,
   TableBody,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
+  Table,
 } from "@/components/ui/table";
 import { Label } from "@/components/ui/label";
 import {
@@ -30,6 +30,7 @@ import {
   type SortingState,
   useReactTable,
   type ColumnFiltersState,
+  type Table as TableType,
 } from "@tanstack/react-table";
 import { useState } from "react";
 import { http } from "@/helpers/axios";
@@ -152,102 +153,126 @@ export default function HomePage() {
 
   return (
     <div className="w-full h-screen">
-      <div className="max-w-5xl m-auto">
-        <div className="p-5">
-          <h1 className="text-3xl">Pasien Masuk Rawat Inap</h1>
-        </div>
-        <div className="p-2">
-          <Input
-            placeholder="Cari nama pasien atau NIK..."
-            value={table.getState().globalFilter ?? ""}
-            onChange={(event) => {
-              table.setGlobalFilter(event.target.value);
-            }}
-            className="max-w-sm"
-          />
-          {/* FORMULIR */}
-          <HealthForm />
-          {/*  */}
-        </div>
-      </div>
-
+      <Header table={table} />
       {!loading ? (
         <div className="overflow-hidden rounded-md border max-w-5xl m-auto">
           {/* TABEL */}
-          <Table>
-            <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <TableHead key={header.id}>
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                      </TableHead>
-                    );
-                  })}
-                </TableRow>
-              ))}
-            </TableHeader>
-            <TableBody>
-              {table.getRowModel().rows?.length ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow
-                    key={row.id}
-                    data-state={row.getIsSelected() && "selected"}
-                  >
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))
-              ) : (
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  Hasil tidak ditemukan.
-                </TableCell>
-              )}
-            </TableBody>
-          </Table>
+          <DataTable table={table} />
           {/*  */}
           {/* PAGINATION */}
-          <div className="flex items-center justify-end space-x-2 py-4">
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              Sebelumnya
-            </Button>
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.nextPage()}
-              disabled={!table.getCanNextPage()}
-            >
-              Berikutnya
-            </Button>
-          </div>
+          <PaginationButton table={table} />
           {/*  */}
         </div>
       ) : (
         // LOADING
-        <div className="max-w-5xl m-auto text-center pt-5">
-          <p>Sedang memperbarui data, harap tunggu...</p>
-        </div>
+        <LoadingComponent />
       )}
+    </div>
+  );
+}
+
+function Header({ table }: { table: TableType<TableEntity> }) {
+  return (
+    <div className="max-w-5xl m-auto">
+      <div className="p-5">
+        <h1 className="text-3xl">Pasien Masuk Rawat Inap</h1>
+      </div>
+      <div className="p-2">
+        {/* Search Form */}
+        <FilterTableSearch table={table} />
+        {/* FORMULIR */}
+        <HealthForm />
+        {/*  */}
+      </div>
+    </div>
+  );
+}
+
+function LoadingComponent() {
+  return (
+    <div className="max-w-5xl m-auto text-center pt-5">
+      <p>Sedang memperbarui data, harap tunggu...</p>
+    </div>
+  );
+}
+
+function FilterTableSearch({ table }: { table: TableType<TableEntity> }) {
+  return (
+    <Input
+      placeholder="Cari nama pasien atau NIK..."
+      value={table.getState().globalFilter ?? ""}
+      onChange={(event) => {
+        table.setGlobalFilter(event.target.value);
+      }}
+      className="max-w-sm"
+    />
+  );
+}
+
+function DataTable({ table }: { table: TableType<TableEntity> }) {
+  return (
+    <Table>
+      <TableHeader>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <TableRow key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
+              return (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </TableHead>
+              );
+            })}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {table.getRowModel().rows?.length ? (
+          table.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && "selected"}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))
+        ) : (
+          <TableCell colSpan={columns.length} className="h-24 text-center">
+            Hasil tidak ditemukan.
+          </TableCell>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
+
+function PaginationButton({ table }: { table: TableType<TableEntity> }) {
+  return (
+    <div className="flex items-center justify-end space-x-2 py-4">
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={() => table.previousPage()}
+        disabled={!table.getCanPreviousPage()}
+      >
+        Sebelumnya
+      </Button>
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={() => table.nextPage()}
+        disabled={!table.getCanNextPage()}
+      >
+        Berikutnya
+      </Button>
     </div>
   );
 }
