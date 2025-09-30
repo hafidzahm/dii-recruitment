c2a087ba695fb14b1a256dd0e93918f351c03bf8
Abdul Hafizh <hafizhahm123@gmail.com>
2025-09-30 14:18:28 +0700

    chore: adding dialog and label component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,6 +6,16 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Label } from "@/components/ui/label";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { DATAS } from "@/datas/datas";
 import type { TableEntity } from "@/types/table";
@@ -24,6 +34,7 @@ import { useState } from "react";
 import { http } from "@/helpers/axios";
 import { Input } from "@/components/ui/input";
 import { ArrowUpDown } from "lucide-react";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 const columnHelper = createColumnHelper<TableEntity>();
 
@@ -124,6 +135,42 @@ export default function HomePage() {
         }}
         className="max-w-sm"
       />
+      <Dialog>
+        <form>
+          <DialogTrigger asChild>
+            <Button variant="outline">Open Dialog</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you&apos;re
+                done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4">
+              <div className="grid gap-3">
+                <Label htmlFor="name-1">Name</Label>
+                <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="username-1">Username</Label>
+                <Input
+                  id="username-1"
+                  name="username"
+                  defaultValue="@peduarte"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </form>
+      </Dialog>
       {!loading ? (
         <div className="overflow-hidden rounded-md border">
           <Table>
